d3aa85be1a7a459287a32b5aa206fce4a7f7908d
Diana-Precious <ambitious.kimberly@yahoo.com>
2023-02-16 14:20:07 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that allows you to print a line using printf function
- * Return: 0 (Success)
+ ** main - A program that allows you to print a line using printf function
+ ** Return: 0 (Success)
  */
 
 int main(void)
